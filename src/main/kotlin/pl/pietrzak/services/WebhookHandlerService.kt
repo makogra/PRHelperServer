4aabfec52b75fac05e968f203e9f2dfcd9c9c748
Maciej Pietrzak <pietrzak.maciek.pv@gmail.com>
2025-09-01 22:06:49 +0200

    Add extractFileDiffs definition for test

--- src/main/kotlin/pl/pietrzak/services/WebhookHandlerService.kt
+++ src/main/kotlin/pl/pietrzak/services/WebhookHandlerService.kt
@@ -1,16 +1,22 @@
 package pl.pietrzak.services
 
-import pl.pietrzak.webhook.GitHubPullRequestPayload
+import pl.pietrzak.entity.github.FileDiff
+import pl.pietrzak.entity.github.GitHubPullRequestPayload
 
 object WebhookHandlerService {
     suspend fun handlePullRequest(payload: GitHubPullRequestPayload) {
         val pr = payload.pullRequest
         val repo = payload.repository
 
-        println("Received PR: ${pr.id} by ${pr.head.ref} on ${repo.name}")
+        println("Received PR: ${pr.id} on ${repo.name}")
 
         // If needed, fetch PR diff with a service:
         // val diff = githubApiService.fetchPullRequestDiff(repo.owner.login, repo.name, pr.number, accessToken)
         // println(diff)
+    }
+
+    fun extractFileDiffs(payload: GitHubPullRequestPayload): List<FileDiff> {
+//        payload.pullRequest.diffUrl
+        return emptyList()
     }
 }
