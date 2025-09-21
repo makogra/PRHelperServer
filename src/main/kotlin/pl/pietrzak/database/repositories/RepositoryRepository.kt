c59369e0aada37c57ab602690c14dfbd9958fe15
Maciej Pietrzak <pietrzak.maciek.pv@gmail.com>
2025-09-21 20:38:08 +0200

    Add Repositories Table

---
+++ src/main/kotlin/pl/pietrzak/database/repositories/RepositoryRepository.kt
@@ -0,0 +1,30 @@
+package pl.pietrzak.database.repositories
+
+import kotlinx.datetime.Clock
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.insertAndGetId
+import org.jetbrains.exposed.sql.transactions.transaction
+import pl.pietrzak.database.tables.Repositories
+import pl.pietrzak.database.tables.RepositoryReviewers
+import java.util.UUID
+
+class RepositoryRepository {
+    fun create(name: String, portalName: String, url: String, creatorId: UUID): Int {
+        return transaction {
+            Repositories.insertAndGetId {
+                it[Repositories.name] = name
+                it[Repositories.portalName] = portalName
+                it[Repositories.url] = url
+                it[Repositories.creator] = creatorId
+                it[Repositories.createdAt] = Clock.System.now()
+            }.value
+        }
+    }
+
+    fun addReviewer(repositoryId: Int, userId: UUID) = transaction {
+        RepositoryReviewers.insert {
+            it[repository] = repositoryId
+            it[user] = userId
+        }
+    }
+}
