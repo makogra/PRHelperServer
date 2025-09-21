04ce92cbf5b397710b903caa57ff21a1341d80ed
Maciej Pietrzak <pietrzak.maciek.pv@gmail.com>
2025-09-21 20:37:19 +0200

    Add User Repository

--- src/main/kotlin/pl/pietrzak/database/repositories/UserRepository.kt
+++ src/main/kotlin/pl/pietrzak/database/repositories/UserRepository.kt
@@ -1,4 +1,26 @@
 package pl.pietrzak.database.repositories
 
+import kotlinx.datetime.Clock
+import org.jetbrains.exposed.sql.and
+import org.jetbrains.exposed.sql.insertAndGetId
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
+import pl.pietrzak.database.tables.Users
+import java.util.*
+
 class UserRepository {
+    fun create(login: String, portalName: String, iconUrl: String?): UUID {
+        return transaction {
+            Users.insertAndGetId {
+                it[Users.login] = login
+                it[Users.portalName] = portalName
+                it[Users.iconUrl] = iconUrl
+                it[Users.createdAt] = Clock.System.now()
+            }.value
+        }
+    }
+
+    fun findByLogin(login: String, portalName: String) = transaction {
+        Users.selectAll().where { (Users.login eq login) and (Users.portalName eq portalName) }.singleOrNull()
+    }
 }
--- src/main/kotlin/pl/pietrzak/database/tables/Users.kt
+++ src/main/kotlin/pl/pietrzak/database/tables/Users.kt
@@ -1,4 +1,11 @@
 package pl.pietrzak.database.tables
 
-object Users {
+import org.jetbrains.exposed.dao.id.UUIDTable
+import org.jetbrains.exposed.sql.kotlin.datetime.timestamp
+
+object Users : UUIDTable("users") {
+    val login = varchar("login", 255).uniqueIndex() // GitHub login or GitLab username
+    val portalName = varchar("portal_name", 50) // e.g. github, gitlab
+    val iconUrl = varchar("icon_url", 512).nullable()
+    val createdAt = timestamp("created_at")
 }
